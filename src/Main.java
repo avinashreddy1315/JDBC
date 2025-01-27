9c01b26428d68355813958f779c7bf2b9fbedea6
Avinahs Reddy <avinashreddy1315@gmail.com>
2025-01-27 14:29:34 -0500

    executed DQL, DML using JDBC

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,48 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        String url = "jdbc:mysql://127.0.0.1:3306/aliens";
+        String uname = "root";
+        String pass = "12345678";
+
+        // Load the MySQL JDBC driver
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        // Establish the connection
+        Connection con = DriverManager.getConnection(url, uname, pass);
+
+        // Create a statement object
+        Statement st = con.createStatement();
+
+
+/*  This is DQL for quering the data
+        // Query to fetch data
+        String query = "SELECT id, username FROM students";
+
+        // Execute the query
+        ResultSet rs = st.executeQuery(query);
+
+        // Loop through the results
+        while (rs.next()) {
+            int id = rs.getInt("id"); // Fetch the id as an integer
+            String username = rs.getString("username"); // Fetch the username as a string
+
+            // Print the result
+            System.out.println(id + " : " + username);
+        } */
+
+
+        String query = "INSERT INTO students (userName) VALUES ('Ram');";
+
+        int count = st.executeUpdate(query);
+        System.out.println(count);
+
+        // Close resources
+
+        st.close();
+        con.close();
     }
 }
